c376a9220494b8d618aa6c65ed2e0bbfd14c085b
Christian Olatomiwa <softwareolatomiwa@gmail.com>
2019-10-17 14:17:43 +0100

    Implemented about activity

--- app/src/main/java/com/example/team46/MainActivity.java
+++ app/src/main/java/com/example/team46/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.team46;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnAbout;
+    Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        context = this;
+
+        btnAbout = findViewById(R.id.btnAbout);
+        btnAbout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, AboutALCActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
