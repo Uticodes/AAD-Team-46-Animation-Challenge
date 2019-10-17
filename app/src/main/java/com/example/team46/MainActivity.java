2709ca48caad1683f0abb5a8f3ec279f41934bc3
southside <opdort24@gmail.com>
2019-10-17 10:57:46 +0100

    add splash screen

--- app/src/main/java/com/example/team46/MainActivity.java
+++ app/src/main/java/com/example/team46/MainActivity.java
@@ -4,11 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import androidx.appcompat.widget.Toolbar;
+
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme_NoActionBar);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        if(getSupportActionBar() != null){
+            getSupportActionBar().setDisplayShowTitleEnabled(true);
+        }
+
     }
 }
